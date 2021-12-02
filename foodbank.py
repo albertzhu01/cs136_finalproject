9d82c126f4cf526867d4aa7acbca3d142dd9d53a
raymondroc <feng.raymond@gmail.com>
2021-12-01 22:50:57 -0500

    .

--- foodbank.py
+++ foodbank.py
@@ -1,14 +1,15 @@
 import math
 import random
 
+
 class FoodBank:
-    def __init__(self, goal_factor, perishable_value, non_perishable_value, budget):
+    def __init__(self, id, goal_factor, perishable_value, non_perishable_value, budget):
+        self.id = id
         self.goal_factor = goal_factor
         self.perishable_value = perishable_value
         self.non_perishable_value = non_perishable_value
         self.budget = budget
+        self.utility = 0
 
-    def bid(self):
-        pass  
-    
-
+    def bid(self, num_items):
+        return [random.uniform(0,1) for _ in range(num_items)]
