bf2df2c7d043fba36404bde8077812dda3d1176d
raymondroc <feng.raymond@gmail.com>
2021-12-02 22:25:50 -0500

    simplify foodbank initialization

--- foodbank.py
+++ foodbank.py
@@ -3,13 +3,14 @@ import random
 
 
 class FoodBank:
-    def __init__(self, id, goal_factor, perishable_value, non_perishable_value, budget):
+    def __init__(self, id, goal_factor, budget):
         self.id = id
         self.goal_factor = goal_factor
-        self.perishable_value = perishable_value
-        self.non_perishable_value = non_perishable_value
         self.budget = budget
         self.utility = 0
+        self.perishable_value = None
+        self.non_perishable_value = None
+
 
     def bid(self, num_items):
         return [random.uniform(0,1) for _ in range(num_items)]
